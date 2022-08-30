659c2c4ffceb29b9e864966b64d6827f098312f0
Benjamin Nomba <bnomba0704@gmail.com>
2022-08-30 08:11:24 -0700

    new file:   .gitignore 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.js 	new file:   src/Components/AddTask.js 	new file:   src/Components/Button.js 	new file:   src/Components/Header.js 	new file:   src/Components/Task.js 	new file:   src/Components/Tasks.js 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/reportWebVitals.js

---
+++ src/Components/Button.js
@@ -0,0 +1,7 @@
+const Button = ({text, add, showAddTask}) => {
+  return (
+    <div className="btn" style={ showAddTask === false ?{background:'blue'} : {background: 'orange'}} onClick={add}>{ showAddTask === false ? text : 'Close'}</div>
+  )
+}
+
+export default Button
---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import Button from "./Button"
+
+const Header = ({title, add, showAddTask}) => {
+  return (
+    <div className="header">
+      <div style={{fontWeight:600}}>{title}</div>
+    <Button text='Add' add={add} showAddTask={showAddTask}/>
+    </div>
+  )
+}
+
+export default Header
